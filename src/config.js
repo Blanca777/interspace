78aa7ae9d89dffdd1093ae9364261a5c9ca917a6
blanca777 <978059629@qq.com>
2021-08-01 16:05:23 +0800

    node地址更换

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const APIUrl = 'http://localhost:1777'
-// export const APIUrl = 'http://blanca777.cn:1777'
+// export const APIUrl = 'http://localhost:1777'
+export const APIUrl = 'http://interspacenode.blanca7.cn:1777'
